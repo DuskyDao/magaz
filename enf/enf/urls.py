8b5e699ea1d88ff0af25bf0a03b0d96fdadb0dc0
DuskyDao <git.duskydog.mane@gmail.com>
2026-02-17 22:53:11 +0200

    add views/urls/admin

--- enf/enf/urls.py
+++ enf/enf/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls", namespace="main")),
 ]
 
 if settings.DEBUG:
